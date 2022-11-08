2470856d43a67d5f7cf359045a75e874348dd874
gayoung-lee0921 <dudnrkdud1@gmail.com>
2022-11-08 10:36:03 +0900

    responsive main-visual

--- js/common.js
+++ js/common.js
@@ -30,3 +30,46 @@ lnbMenu.on("click", function (e) {
   const siblings = $(this).next();
   siblings.stop().fadeToggle();
 });
+
+const btnAll = $(".all-menu");
+const depth01 = $("#gnb .depth01");
+
+btnAll.on("click", function () {
+  gnb.toggleClass("on");
+  const icon = $(this).find("i");
+  if (gnb.hasClass("on")) {
+    icon.removeClass("fa-bars").addClass("fa-xmark");
+  } else {
+    icon.removeClass("fa-xmark").addClass("fa-bars");
+  }
+});
+
+$(window).on("resize", function () {
+  const w = $(window).outerWidth();
+  if (w > 1280) {
+    $("html").addClass("pc").removeClass("m");
+    $("#gnb .depth02").removeAttr("style");
+    $("#gnb").removeClass("on");
+    const icon = $(".all-menu i");
+    icon.removeClass("fa-xmark").addClass("fa-bars");
+  } else {
+    $("html").addClass("m").removeClass("pc");
+  }
+});
+
+$(window).trigger("resize");
+
+depth01.on("click", function (e) {
+  if ($("html").hasClass("m")) {
+    const depth02 = $(this).next();
+    const siblings = $(this).parent().siblings().find(".depth02");
+    const siblingsDepth01 = $(this).parent().siblings().find(".depth01");
+    $(this).toggleClass("on");
+    siblingsDepth01.removeClass("on");
+    siblings.slideUp();
+    if (depth02.length > 0) {
+      e.preventDefault();
+      depth02.stop().slideToggle();
+    }
+  }
+});
